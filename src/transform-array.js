828852d0f8d4338e27596d84331ae487ce2a9e7b
endezz <r-serega13@yandex.ru>
2021-03-28 17:02:05 +0400

    feat: complite transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error();
+  let isCheck = false;
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next":
+        i++;
+        isCheck = true;
+        break;
+      case "--discard-prev":
+        if (res.length > 0 && !isCheck) res.pop();
+        isCheck = true;
+        break;
+      case "--double-next":
+        if (i + 1 < arr.length) res.push(arr[i + 1]);
+        isCheck = true;
+        break;
+      case "--double-prev":
+        if (i !== 0 && !isCheck) res.push(arr[i - 1]);
+        isCheck = true;
+        break;
+      default:
+        res.push(arr[i]);
+        isCheck = false;
+    }
+  }
+  return res;
 };
