3538dd60b18bb10595a73268c6e850aeb7500a09
endezz <r-serega13@yandex.ru>
2021-03-28 16:44:23 +0400

    feat: complite extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,18 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  str = String(str);
+  let separator = String(options.separator);
+  let repeatTimes = options.repeatTimes;
+  let additionSeparator = String(options.additionSeparator);
+  let addition = String(options.addition);
+  let additionRepeatTimes = options.additionRepeatTimes;
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (separator === "undefined") separator = "+";
+  if (additionSeparator === "undefined") additionSeparator = "|";
+  if (addition === "undefined") addition = "";
+  if (repeatTimes === "undefined") repeatTimes = 1;
+  if (additionRepeatTimes === "undefined") additionRepeatTimes = 1;
+  if (typeof addition !== "undefined")
+    str +=
+      addition + (additionSeparator + addition).repeat(additionRepeatTimes - 1);
+  return str.concat(separator.concat(str).repeat(repeatTimes - 1));
 };
-  
